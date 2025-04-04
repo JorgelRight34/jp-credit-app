2751fb8576ba2115f43ec6ac07713a5b18df237a
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-04-04 08:03:45 -0400

    Before pulling

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef6ad530cd136306354a462a59d7ba30d1b4463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8aa30e05a6b3581b336ee31829255c03016c63")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
