04f131d5c176853d439a3ed3eea8a18ff4f7566d
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-04-14 08:11:43 -0400

    pulling

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4318752ac2c717031692a3820c9ae9bc306f0232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640e54f31566a246b39aedfd372d152c4ea9145f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
