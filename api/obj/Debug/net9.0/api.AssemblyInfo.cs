182c9d81f26d35759d4bef6e1d195533476a76e4
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-04-15 07:59:54 -0400

    merging

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c07554f9cb30369f83904b10ee211991433fb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e3618e522ad339d69ac905ee613228ad1daf40")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
